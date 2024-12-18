6ea2220a405df764f5df7b31fe86a567ca96c649
sophia-martin-psc <sophia.martin@thepsc.co.uk>
2024-12-18 15:50:17 +0000

    Created resources to allow local testing of jinja templates

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # monitor-space-hazards-email
 Monitor Space Hazards - email templates for AWS SES
+
+## Technical documentation
+
+### Preview rendering of jinja email templates 
+You will need to ensure you have python installed. You can download it via the Microsoft Store. 
+
+You will also need the python package jinja2. You can install it by running `python -m pip install jinja2`
+
+Open the file testing-resources/render_html_from_template.py in IDLE. Change file_to_test to the name of the file you'd like to render, and then run the program. 
---
+++ testing-resources/render_html_from_template.py
@@ -0,0 +1,44 @@
+from jinja2 import Environment, FileSystemLoader
+import json
+
+
+file_to_test = "scheduled-outage.jinja" # change this value to test different files
+
+
+def to_risk(probability):
+    return "Low"
+
+def to_affected_territories(x):
+    return "Wales"
+
+def to_time_frame(x):
+    return "10:00 - 12:00"
+
+def to_full_country_name(code):
+    return "US"
+
+environment = Environment(loader=FileSystemLoader("..\\html"))
+
+environment.filters['to_risk'] = to_risk
+environment.filters['to_affected_territories'] = to_affected_territories
+environment.filters['to_time_frame'] = to_time_frame
+environment.filters['to_full_country_name'] = to_full_country_name
+
+
+template = environment.get_template(file_to_test)
+
+
+with open('test_json_data.json', 'r') as file:
+    json_string = file.read()
+
+
+my_dict = student_details = json.loads(json_string)
+
+filename = "test.html"
+content = template.render(
+    **my_dict
+)
+with open(filename, mode="w", encoding="utf-8") as message:
+    message.write(content)
+    print(f"Created {filename} in testing-resources folder. Open this file to preview email template")
+
