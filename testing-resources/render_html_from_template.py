e4739dcc5f188db10b6437984114706130828168
sophia-martin-psc <sophia.martin@thepsc.co.uk>
2025-03-17 16:42:21 +0000

    Hide unknown in header, remove date confirmation from close down

--- testing-resources/render_html_from_template.py
+++ testing-resources/render_html_from_template.py
@@ -1,9 +1,8 @@
 from jinja2 import Environment, FileSystemLoader
 import json
+from datetime import datetime
 
-
-file_to_test = "re-entry-alert-updated.jinja" # change this value to test different files
-
+file_to_test = "re-entry-alert-updated.jinja"  # Change this value to test different files
 
 def to_risk(probability):
     return "Low"
@@ -24,21 +23,22 @@ environment.filters['to_affected_territories'] = to_affected_territories
 environment.filters['to_time_frame'] = to_time_frame
 environment.filters['to_full_country_name'] = to_full_country_name
 
-
 template = environment.get_template(file_to_test)
-
 
 with open('test_json_data.json', 'r') as file:
     json_string = file.read()
 
+my_dict = json.loads(json_string)
 
-my_dict = student_details = json.loads(json_string)
+# Convert decay_time to a datetime object if present in the JSON data
+if "decay_time" in my_dict:
+    my_dict["decay_time"] = datetime.fromisoformat(my_dict["decay_time"])
 
 filename = "test.html"
 content = template.render(
     **my_dict
 )
+
 with open(filename, mode="w", encoding="utf-8") as message:
     message.write(content)
     print(f"Created {filename} in testing-resources folder. Open this file to preview email template")
-
