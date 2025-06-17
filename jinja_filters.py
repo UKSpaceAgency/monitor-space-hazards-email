709914c0d0193d7dd936cf8ddfb68e07af97e90a
Philip <nowakf@gmail.com>
2025-06-17 11:06:34 +0100

    updated filter for affected territiories

--- jinja_filters.py
+++ jinja_filters.py
@@ -1,12 +1,76 @@
 from datetime import datetime
 from typing import Any
 
+jsonRegionsMap = {
+  "england_nation": 'England',
+  "northern_ireland_nation": "Northern Ireland",
+  "scotland_nation": "Scotland",
+  "wales_nation": "Wales",
+  "east_of_england": "East of England",
+  "east_midlands": "East Midlands",
+  "london": "London",
+  "north_east": "North East",
+  "north_west": "North West",
+  "south_east": "South East",
+  "south_west": "South West",
+  "west_midlands": "West Midlands",
+  "yorkshire_and_north_humber": "Yorkshire and North Humber",
+  "antrim_and_newtownabbey": "Antrim and Newtownabbey",
+  "ards_and_north_down": "Ards and North Down",
+  "armagh_city_banbridge_and_craigavon": "Armagh City, Banbridge and Craigavon",
+  "belfast": "Belfast",
+  "causeway_coast_and_glens": "Causeway Coast and Glens",
+  "derry_and_strabane": "Derry and Strabane",
+  "fermanagh_and_omagh": "Fermanagh and Omagh",
+  "lisburn_and_castlereagh": "Lisburn and Castlereagh",
+  "mid_and_east_antrim": "Mid and East Antrim",
+  "mid_ulster": "Mid Ulster",
+  "newry_mourne_and_down": "Newry, Mourne and Down",
+  "central_scotland": "Central Scotland",
+  "glasgow": "Glasgow",
+  "highlands_and_islands": "Highlands and Islands",
+  "lothian": "Lothian",
+  "mid_scotland_and_fife": "Mid Scotland and Fife",
+  "north_east_scotland": "North East Scotland",
+  "south_scotland": "South Scotland",
+  "west_scotland": "West Scotland",
+  "north_wales": "North Wales",
+  "mid_wales": "Mid Wales",
+  "south_west_wales": "South West Wales",
+  "south_east_wales": "South East Wales",
+  "anguilla": "Anguilla",
+  "bermuda": "Bermuda",
+  "british_antarctic_territory": "British Antarctic Territory",
+  "british_indian_ocean_territory": "British Indian Ocean Territory",
+  "british_virgin_islands": "British Virgin Islands",
+  "cayman_islands": "Cayman Islands",
+  "falkland_islands": "Falkland Islands",
+  "gibraltar": "Gibraltar",
+  "montserrat": "Montserrat",
+  "pitcairn_islands": "Pitcairn Islands",
+  "saint_helena_ascension_and_tristan_da_cunha": "Saint Helena, Ascension and Tristan da Cunha",
+  "south_georgia_and_the_south_sandwich_islands": "South Georgia and the South Sandwich Islands",
+  "sovereign_base_areas_of_akrotiri_and_dhekelia": "Sovereign Base Areas of Akrotiri and Dhekelia",
+  "turks_and_caicos_islands": "Turks and Caicos Islands",
+  "isle_of_man": "Isle of Man",
+  "jersey": "Jersey",
+  "guernsey": "Guernsey",
+  "uk_navarea": "UK NAVAREA",
+  "shanwick_airspace": "Shanwick Oceanic FIR",
+  "shanwick_oceanic_fir": "Shanwick Oceanic FIR",
+  "london_fir": "London FIR",
+  "scotland_fir": "Scotland FIR",
+};
 
 def to_affected_territories(value: dict[str, dict[str, Any]]) -> str:
     """England, Wales"""
+    # return ", ".join(
+    #     [k.replace("_nation", "").replace("_", " ").title() for k, v in value.items() if v["probability"] > 0.0]
+    # )
     return ", ".join(
-        [k.replace("_nation", "").replace("_", " ").title() for k, v in value.items() if v["probability"] > 0.0]
+        [jsonRegionsMap[k] for k, v in value.items() if v["probability"] > 0.0]
     )
+    
 
 
 def to_time_frame(value: list[str | datetime]) -> str:
